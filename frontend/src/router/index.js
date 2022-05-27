3e87866ebe10b73853c85ea26627f7dd95612bc4
Amalia Hajarani <lalamalia2103@gmail.com>
2022-05-27 15:51:12 +0700

    :sparkles: Adding Agenda Page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import HalamanBeranda from '../views/HalamanBeranda.vue'
 import AboutView from '../views/AboutView.vue'
 import HalamanSaranaPrasarana from '../views/HalamanSaranaPrasarana.vue'
+import HalamanAgenda from '../views/HalamanAgenda.vue'
 
 Vue.use(VueRouter)
 
@@ -21,7 +22,12 @@ const routes = [
     path: '/sarana-prasarana',
     name: 'sarana-prasarana',
     component: HalamanSaranaPrasarana
-  }
+  },
+  {
+    path: '/agenda',
+    name: 'agenda',
+    component: HalamanAgenda
+  },
 ]
 
 const router = new VueRouter({
