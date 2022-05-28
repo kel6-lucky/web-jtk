3525661d069ce5f9bf5718040e89e4c38916e7de
FarraJessica <farrajessica@gmail.com>
2022-05-28 21:00:31 +0700

    :lipstick: Update informasi halaman Profil Prodi D3 dan D4

---
+++ frontend/src/components/TableDataPrestasi.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-app>
+        <div class="table-style">
+            <v-data-table
+            :headers="headers"
+            :items="mata_kuliah"
+            class="elevation-1"
+            :itemsPerPage="5"
+            :page.sync="page"
+            @page-count="pageCount = $event"
+            hide-default-footer
+            >
+            </v-data-table>
+            <div class="text-center mt-8">
+                <v-pagination
+                v-model="page"
+                :length="pageCount"
+                ></v-pagination>
+            </div>
+        </div>
+    </v-app>
+</template>
+
+<script>
+// import axios from "axios";
+export default {
+    name: 'TableDataPrestasi',
+    data() {
+        return {
+            page: 1,
+			pageCount: 0,
+            headers: [
+                { text: 'Nama Peraih', sortable: false, value: 'Nama_Peraih'},
+                { text: 'Kategori', sortable: false, value: 'Kategori' },
+                { text: 'Nama Kejuaraan', sortable: false, value: 'Nama Kejuaraan' },
+                { text: 'Tingkatan', sortable: false, value: 'Tingkatan' },
+            ],
+            data_prestasi: []
+        }
+    },
+    // mounted() {
+    //     axios
+    //         .get("http://localhost:1337/mata-kuliahs?_where[0][Program_Studi]=D3_TI")
+    //         .then(response => {
+    //         this.data_prestasi = response.data;
+    //     })
+    // }
+}
+</script>
+
+<style>
+.v-application--wrap {
+    min-height: auto;
+}
+.table-style {
+    font-family: 'Lato', sans-serif;
+    border-collapse: collapse;
+}
+th {
+    text-align: center;
+    background: #1F3C88;
+    cursor: pointer;
+}
+.table-style th span {
+    color: #FFFFFF;
+}
+td {
+    border: 1px solid #1F3C88;
+}
+</style>
